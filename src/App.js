9c5c964c3bd8deee69a3fcf3006c92a1020034f2
HarshTechnical <hpandey682@gmail.com>
2021-09-28 13:11:04 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+
+import ClzContext from './component/Home/clzContext';
+
+import ClzName from './component/Home/clzName';
+
+const App = () => {
+
+
+  return (
+    <>
+    {/* <div className="box3"> <spna><a href="http://localhost:3000/">Home</a></spna></div> */}
+      <ClzName />
+      
+<div className="center" >
+      <style>{'body { background-color: #FFFFF0; }'}</style>
+    
+    
+   <ClzContext />
+    
+      </div>
+      
+  </>
+     
+    
+    
+    );
+  };
+    
+    
+ 
+
+
+
+export default App;
